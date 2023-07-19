ccc0d05bea5c5d62a4c66c524a749d1335147a9e
sergey <szaprudnov@hotmail.com>
2023-07-20 02:46:56 +0300

    add class Client

--- my_work/models.py
+++ my_work/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Client(models.Model):
+    email = models.EmailField(verbose_name='Email клиента', unique=True)
+    name = models.CharField(max_length=150, verbose_name='ФИО')
+    comment = models.TextField(verbose_name='Комментарий')
+
+    def __str__(self):
+        return f'{self.email}'
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
