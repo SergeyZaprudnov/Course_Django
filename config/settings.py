6fa5893b92070b549841f45a0f2eefce85e7449a
sergey <szaprudnov@hotmail.com>
2023-07-20 09:42:08 +0300

    editing

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'my_work',
 ]
 
 MIDDLEWARE = [
@@ -72,8 +73,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Course_Django',
+        'USER': 'postgres',
+        'PASSWORD': 'sixteen',
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
     }
 }
 
