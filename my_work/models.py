33950d108188af6de9dd44c31e4244f5081686f5
sergey <szaprudnov@hotmail.com>
2023-07-20 09:25:30 +0300

    add class Newsletter

--- my_work/models.py
+++ my_work/models.py
@@ -14,3 +14,31 @@ class Client(models.Model):
     class Meta:
         verbose_name = 'Клиент'
         verbose_name_plural = 'Клиенты'
+
+
+class Newsletter(models.Model):
+    SEND_FREQUENCY_CHOICES = (
+        ('create', 'Создана'),
+        ('running', 'Запущена'),
+        ('completed', 'Завершена'),
+    )
+    STATUS_CHOICES = (
+        ('created', 'Создана'),
+        ('running', 'Запущена'),
+        ('completed', 'Завершена'),
+    )
+
+    subject = models.CharField(max_length=100, verbose_name='Тема')
+    body = models.TextField(verbose_name='Содержимое')
+    frequency = models.CharField(max_length=50, choices=SEND_FREQUENCY_CHOICES, verbose_name='Периодичность')
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='created', verbose_name='Статус')
+    customers = models.ManyToManyField('Customer', verbose_name='Клиенты', related_name='newsletters')
+
+    def __str__(self):
+        return f'{self.subject}'
+
+    class Meta:
+        verbose_name = 'Рассылка'
+        verbose_name_plural = 'Рассылки'
+
+
