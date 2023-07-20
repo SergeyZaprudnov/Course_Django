6b3f5ab5a4f4ab572a54703cb23af13d8c257275
sergey <szaprudnov@hotmail.com>
2023-07-20 19:49:41 +0300

    editing

--- user/models.py
+++ user/models.py
@@ -35,7 +35,7 @@ class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='Почта')
     phone = models.CharField(max_length=35, verbose_name='Телефон', **NULLABLE)
-    avatar = models.ImageField(upload_to='users/', verbose_name='Аватар', **NULLABLE)
+    avatar = models.ImageField(upload_to='user/', verbose_name='Аватар', **NULLABLE)
     country = models.CharField(max_length=100, verbose_name='Страна', **NULLABLE)
 
     is_active = models.BooleanField(default=True, verbose_name='Подтверждение почты')
