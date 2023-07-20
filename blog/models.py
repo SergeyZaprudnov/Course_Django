f68c453a3a87cf4484dcc9b5033c1b5e0eaea8be
sergey <szaprudnov@hotmail.com>
2023-07-20 10:12:39 +0300

    add model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    content = models.TextField(verbose_name='Текст контента')
+    image = models.ImageField(upload_to='blog_images/', verbose_name='Изображение', **NULLABLE)
+    views = models.IntegerField(default=0, verbose_name='Просмотры')
+    pub_date = models.DateField(default=timezone.now, verbose_name='Дата публикации')
+    is_active = models.BooleanField(default=False, verbose_name='активно')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Публикация в блоге'
+        verbose_name_plural = 'Публикации в блоге'
